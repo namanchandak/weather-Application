c9b48c7a6883e60584e94626632e29b5302b5b14
namanchandak <naman.chandak@hotmail.com>
2023-03-25 19:27:02 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _LandingPageState extends State<LandingPage> {
               children: [
                 Container(
                   padding: const EdgeInsets.all(15),
-                  height: 150,
+                  height: 200,
                   color: Colors.black26,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -83,13 +83,157 @@ class _LandingPageState extends State<LandingPage> {
                           ),
                           Icon(Icons.sunny, size: 24, color: Colors.yellow),
                         ],
+                      ),
+
+                          Text("Few Clouds",
+                          style: TextStyle(fontSize: 24,fontWeight: FontWeight.w500),
+                          ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("Updated on",
+                          style:TextStyle(fontSize: 14,fontWeight: FontWeight.bold ) ,
+                          )
+
+
+                        ],
                       )
                     ],
                   ),
+                ),
+
+                Container(
+                  height: 150,
+                  color: Colors.black26,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.water_drop_outlined),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Humidity",
+                          style: TextStyle(fontSize: 24),),
+                          Text("33%",
+                            style: TextStyle(fontSize: 15),
+                          ),
+
+                        ],
+                      ),
+
+
+
+                      Icon(Icons.remove_red_eye_outlined),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Visibility",
+                            style: TextStyle(fontSize: 24),),
+                          Text("6000Km",
+                            style: TextStyle(fontSize: 15),
+                          ),
+
+
+
+                        ],
+                      ),
+
+
+
+
+                      Icon(Icons.monitor_weight_outlined),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("pressuer",
+                            style: TextStyle(fontSize: 24),),
+                          Text("1011Pa",
+                            style: TextStyle(fontSize: 15),
+                          ),
+
+
+
+                        ],
+                      ),
+
+
+
+
+
+
+                    ],
+
+
+
+                  ),
+
+
+
+
+
+
+                ),
+//////////////3rd containor
+                Container(
+                  height: 150,
+                  color: Colors.black26,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CustomRow(icon: Icons.wind_power_outlined,title: "Wind",value: "3m/s",),
+                      CustomRow(icon: Icons.wind_power_outlined,title: "Min Temp",value: "32",),
+                      CustomRow(icon: Icons.wind_power_outlined,title: "Wind",value: "3m/s",),
+
+                    ],
+                  ),
+
+
+
+
+
+
                 )
+
+
               ],
             ),
           ),
         ));
+  }
+}
+
+class CustomRow extends StatelessWidget {
+  const CustomRow({
+    super.key, required this.icon, required this.title, required this.value,
+  });
+
+  final IconData icon;
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+
+        Icon(icon),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(title,
+              style: TextStyle(fontSize: 24),),
+            Text(value,
+              style: TextStyle(fontSize: 15),
+            ),
+
+          ],
+        ),
+
+
+      ],
+
+    );
   }
 }
